0387745f47a72b11baceb199b7dcaa1ac3a46033
LRomero2 <lauren.romero@outlook.com>
2020-11-23 09:27:56 +0000

    Added more to the new template im working on.

--- assets/js/app.js
+++ assets/js/app.js
@@ -133,335 +133,141 @@ startBtn.addEventListener('click', countDown)
 
 
 
+var symbols = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', 'anchor', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond'],
+		opened = [],
+		match = 0,
+		moves = 0,
+		$deck = $('.deck'),
+		$scorePanel = $('#score-panel'),
+		$moveNum = $scorePanel.find('.moves'),
+		$ratingStars = $scorePanel.find('i'),
+		$restart = $scorePanel.find('.restart'),
+		delay = 800,
+		gameCardsQTY = symbols.length / 2,
+		rank3stars = gameCardsQTY + 2,
+		rank2stars = gameCardsQTY + 6,
+		rank1stars = gameCardsQTY + 10;
 
-
-
-
-
-
-
-let matchedCards     = [],
-    currentOpenedCards = [],
-    moves            = 0,
-    rateHTML = "",
-    rateStep = 6,
-    firstClick = true,
-    minutes, seconds,
-    totalTime = 0,
-    incrementer;
-
-const iconsList          = ["shop", "shop", "coffee", "coffee", "i went", "i went", "to the", "to the", "to", "to", "buy", "buy"],
-      cardsList          = document.querySelector(".memory-cards"),
-      cards              = cardsList.children,
-      movesContainer     = document.querySelector(".score"),
-      modal              = document.querySelector(".modal"),
-      repeatBtnFromModal = document.querySelector(".modal .play-again"),
-      exactMoves         = iconsList.length / 2,
-      secondsContainer = document.querySelector("#seconds"),
-      minutesContainer = document.querySelector("#minutes"),
-
-/*
- * Shuffle
- */
+// Shuffle function From http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let counter = array.length;
+  var currentIndex = array.length, temporaryValue, randomIndex;
+	
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
+  return array;
+}
 
-        // Decrease counter by 1
-        counter--;
+// Initial Game
+function initGame() {
+	var cards = shuffle(symbols);
+  $deck.empty();
+  match = 0;
+  moves = 0;
+  $moveNum.html(moves);
+  $ratingStars.removeClass('fa-star-o').addClass('fa-star');
+	for (var i = 0; i < cards.length; i++) {
+		$deck.append($('<li class="card"><i class="fa fa-' + cards[i] + '"></i></li>'))
+	}
+};
 
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+// Set Rating and final Score
+function setRating(moves) {
+	var rating = 3;
+	if (moves > rank3stars && moves < rank2stars) {
+		$ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
+		rating = 2;
+	} else if (moves > rank2stars && moves < rank1stars) {
+		$ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
+		rating = 1;
+	} else if (moves > rank1stars) {
+		$ratingStars.eq(0).removeClass('fa-star').addClass('fa-star-o');
+		rating = 0;
+	}	
+	return { score: rating };
+};
+
+// End Game
+function endGame(moves, score) {
+	swal({
+		allowEscapeKey: false,
+		allowOutsideClick: false,
+		title: 'Congratulations! You Won!',
+		text: 'With ' + moves + ' Moves and ' + score + ' Stars.\nBoom Shaka Lak!',
+		type: 'success',
+		confirmButtonColor: '#9BCB3C',
+		confirmButtonText: 'Play again!'
+	}).then(function(isConfirm) {
+		if (isConfirm) {
+			initGame();
+		}
+	})
+}
+
+// Restart Game
+$restart.on('click', function() {
+  swal({
+    allowEscapeKey: false,
+    allowOutsideClick: false,
+    title: 'Are you sure?',
+    text: "Your progress will be Lost!",
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#9BCB3C',
+    cancelButtonColor: '#EE0E51',
+    confirmButtonText: 'Yes, Restart Game!'
+  }).then(function(isConfirm) {
+    if (isConfirm) {
+      initGame();
     }
+  })
+});
 
-    return array;
-}
+// Card flip
+$deck.on('click', '.card:not(".match, .open")', function() {
+	if($('.show').length > 1) { return true; }
+	
+	var $this = $(this),
+			card = $this.context.innerHTML;
+  $this.addClass('open show');
+	opened.push(card);
 
-/*
- * Init Game
- */
-function init() {
-    // Shuffle the current `iconsList`
-    const icons = shuffle(iconsList);
-    // Using `createDocumentFragment` for a better performance, but you can `appendChild` to the parent directly!
-    const cardsFragment = document.createDocumentFragment();
-    
-    /*
-     *
-     * 1) Create the cards based on the given length of `icons` array
-     * 2) Put a random icon in each card
-     * 3) Append the created cards to the container `cardsList`
-     *
-     */
-    for (let i = 0; i < icons.length; i++) {
-        const card = document.createElement("li");
-        card.innerHTML = "<i class='" + icons[i] + "'></i>";
-        cardsFragment.appendChild(card);
-    }
-    cardsList.appendChild(cardsFragment);
-}
-
-/*
- * Start Game
- */
-function start() {
-    
-    // Initialize Game
-    init();
-
-    // Start the 'Click' Functionality
-    cardClick();
-}
-
-/* 
- * Click
- */
-function cardClick() {
-    for (let i = 0; i < cards.length; i++) {
-        
-        // Add a "click" event to each card [ The following function will only be executed if a card was clicked! ]
-        cards[i].addEventListener("click", function () {
-
-            // Cache the `current` and `previous` clicked cards
-            const currentCard = this; // current card
-            const previousCard = currentOpenedCards[0]; // the previous one
-            
-            // The First Click? Start the timer!
-            if(firstClick) {
-                startTimer();
-                firstClick = false; // This will prevent the timer to start again if the user clicked on another card
-            }
-
-            /*
-             *
-             * If we have ONE card inside our `currentOpenedCards`
-             * Do the following:
-             *
-             * 1) Make sure that the user didn't click on the same card twice
-             * 2) Compare the `current` clicked card with the exisiting one on our `currentOpenedCards` array
-             * 3) Delete both cards from our `currentOpenedCards`
-             * 4) Add a move
-             * 5) Change the rating
-             *
-             */
-            if (currentOpenedCards.length === 1) {
-
-                /* 
-                 *
-                 * show: Allows you to see the icon
-                 * disabled: To avoid clicking on the same card again!
-                 * animated & flipInY: Both of the are `animate.css` classes for a nicer animation effect
-                 *
-                 */ 
-                currentCard.className = "show disabled animated flipInY";
-
-                // Add this card to the `currentOpenedCards` array
-                currentOpenedCards.push(currentCard);
-
-                // Compare the `current` card with the `previous` card (the first one in `currentOpenedCards` array) 
-                isMatched(currentCard, previousCard);
-
-                /*
-                 *
-                 * Empty `currentOpenedCards`
-                 *
-                 * The `currentOpenedCards` array MUST be empty
-                 * Next time the user click on a card, that card would become the first index,
-                 * Then, when click on another one, both of them will be compared,
-                 * If they are MATCHED or NOT, that doesn't matter here.
-                 * We have to reset the `currentOpenedCards` array, to start filling it again with 2 new clicked cards!
-                 *
-                 */
-                currentOpenedCards = [];
-
-                // Add a move
-                addMove();
-
-                // Change the rating
-                rating();
-
-            } else {
-                
-                /*
-                 *
-                 * If we don't have any card inside our `currentOpenedCards` array
-                 * Do the following:
-                 *
-                 * 1) Add the follwing CSS Classes: 
-                 *    - show: To see the icon inside your card
-                 *    - disabled: To avoid clicking on the same card again!
-                 *    - animated & flipInY: Both of the are `animate.css` classes for a nicer animation effect
-                 * 2) Add this card to the `currentOpenedCards` array
-                 * 
-                 */
-                currentCard.className = "show disabled animated flipInY";
-                currentOpenedCards.push(currentCard);
-            }
-        });
-    }
-}
-
-// Compare the 2 opened cards
-function isMatched(currentCard, previousCard) {
-    
-    /* 
-     * Matched?
-     * Do the following:
-     * 
-     * 1) Change them to success state:
-     *    - Keep displaying the icon
-     *    - Animate
-     *    - Disable them (cannot be clickable anymore)
-     * 2) Add both cards to `matchedCards` array
-     * 3) Check if the game is over or not
-     *
-     */
-    if (currentCard.innerHTML === previousCard.innerHTML) {
-
-        // Change them to success state
-        currentCard.className = "show matched animated bounceOutDown disabled";
-        previousCard.className = "show matched animated bounceOutDown disabled";
-
-        // Add Current & Previous card to `matchedCards` array
-        matchedCards.push(currentCard, previousCard);
-
-        // Game Over?
-        isOver();
-
+	// Compare with opened card
+  if (opened.length > 1) {
+    if (card === opened[0]) {
+      $deck.find('.open').addClass('match animated infinite rubberBand');
+      setTimeout(function() {
+        $deck.find('.match').removeClass('open show animated infinite rubberBand');
+      }, delay);
+      match++;
     } else {
-        
-        /*
-         * Not Matched?
-         * Do the following:
-         *
-         * 1) Back both cards to the normal state:
-         *    - Hide the icon
-         *    - Animate
-         *
-         */
-        setTimeout(function () {
-            // Use `className` to replace existing classes with the given ones
-            currentCard.className = "animated jello";
-            previousCard.className = "animated jello";
-        }, 500)
-
+      $deck.find('.open').addClass('notmatch animated infinite wobble');
+			setTimeout(function() {
+				$deck.find('.open').removeClass('animated infinite wobble');
+			}, delay / 1.5);
+      setTimeout(function() {
+        $deck.find('.open').removeClass('open show notmatch animated infinite wobble');
+      }, delay);
     }
-}
+    opened = [];
+		moves++;
+		setRating(moves);
+		$moveNum.html(moves);
+  }
+	
+	// End Game if match all cards
+	if (gameCardsQTY === match) {
+		setRating(moves);
+		var score = setRating(moves).score;
+		setTimeout(function() {
+			endGame(moves, score);
+		}, 500);
+  }
+});
 
-
-/*
- * Add a move
- */
-function addMove() {
-    moves++;
-    movesContainer.innerHTML = moves;
-}
-
-
-/*
- * Game Over?
- */
-function isOver() {
-    // Check if the `matchedCards` length equals to the `iconsList` array
-    if (iconsList.length === matchedCards.length) {
-        // If it is over, display a popup message!
-        gameOverMessage();
-    }
-}
-
-/* 
- * Game Over Message
- */
-function gameOverMessage() {
-
-    // Display the modal
-    modal.style.top = "0";
-
-    // Add moves to the Modal
-    const totalMoves = document.querySelector("#total_moves");
-    totalMoves.innerHTML = moves + 1; // [bug]: `moves` returns the count - 1
-
-    // Add Rate
-    rateContainer.innerHTML = rateHTML;
-
-    // Stop Timer
-    stopTimer();
-
-    // Add time to the Modal
-    const totalMinutes     = document.querySelector("#totalMinutes");
-    const totalSeconds     = document.querySelector("#totalSeconds");
-    totalMinutes.innerHTML = minutes;
-    totalSeconds.innerHTML = seconds;
-
-}
-
-
-
-  Timer [ Start ] 
- 
-function startTimer() {
-
-    // Start Incrementer
-    incrementer = setInterval(function() {
-
-        // Add totalTime by 1
-        totalTime += 1;
-
-        // Convert Total Time to H:M:S
-        calculateTime(totalTime);
-
-        // Change the current time values
-        secondsContainer.innerHTML = seconds;
-        minutesContainer.innerHTML = minutes;
-        hoursContainer.innerHTML   = hours;
-
-    }, 1000);
-
-    
-}
-
-/*
- * Timer [ Calculate Time ] 
- */
-function calculateTime(totalTime) {
-    hours   = Math.floor( totalTime / 60 / 60);
-    minutes = Math.floor( (totalTime / 60) % 60);
-    seconds = totalTime % 60;
-}
-
-/*
- * Timer [ Stop ] 
- */
-function stopTimer() {
-    // Stop Timer
-    clearInterval(incrementer);
-}
-
-
-/*
- * Reset Current Values
- */
-function resetValues() {
-    matchedCards = [];
-    currentOpenedCards = [];
-    moves = 0;
-    movesContainer.innerHTML = "--";
-    stars[1].style.color = "#ffb400";
-    stars[2].style.color = "#ffb400";
-    rateHTML = "";
-    hoursContainer.innerHTML = "00";
-    minutesContainer.innerHTML = "00";
-    secondsContainer.innerHTML = "00";
-    stopTimer();
-    firstClick = true;
-    totalTime = 0;
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
-}
+initGame();
