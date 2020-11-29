0adf0e198de0aa179f942471838f711ab9d24653
LRomero2 <lauren.romero@outlook.com>
2020-11-29 15:15:13 +0000

    Added data for the cards in a json format.

---
+++ assets/data.js
@@ -0,0 +1,123 @@
+const cardData = [
+ {
+   name: "Englishbuy",
+   image: "englishbuy.jpg",
+   id: "englishbuy"
+ },
+ {
+   name: "Englishcoffee",
+   image: "englishcoffee.jpg",
+   id: "englishcoffee"
+ },
+ {
+   name: "Englishiwent",
+   image: "englishiwent.jpg",
+   id: "englishiwent"
+ },
+ {
+   name: "Englishshop",
+   image: "englishshop.jpg",
+   id: "englishshop"
+ },
+ {
+   name: "Englishto",
+   image: "englishto.jpg",
+   id: "englishto"
+ },
+ {
+   name: "Englishtothe",
+   image: "englishtothe.jpg",
+   id: "englishtothe"
+ },
+ {
+   name: "Portuguesebuy",
+   image: "portuguesebuy.jpg",
+   id: "portuguesebuy"
+ },
+ {
+   name: "Portuguesecoffee",
+   image: "portuguesecoffee.jpg",
+   id: "portuguesecoffee"
+ },
+ {
+   name: "Portugueseiwent",
+   image: "portugueseiwent.jpg",
+   id: "portugueseiwent"
+ },
+ {
+   name: "Portugueseshop",
+   image: "portugueseshop.jpg",
+   id: "portugueseshop"
+ },
+ {
+   name: "Portugueseto",
+   image: "portugueseto.jpg",
+   id: "portugueseto"
+ },
+ {
+   name: "Portuguesetothe",
+   image: "portuguesetothe.jpg",
+   id: "portuguesetothe"
+ },
+ {
+   name: "Portuguesetoday",
+   image: "portuguesetoday.jpg.jpg",
+   id: "portuguesetoday"
+ },
+ {
+   name: "Englishtoday",
+   image: "englishtoday.jpg.jpg",
+   id: "englishtoday"
+ },
+ {
+   name: "Portugueseimgoingtogo",
+   image: "portugueseimgoingtogo.jpg.jpg",
+   id: "portugueseimgoingtogo"
+ },
+ {
+   name: "Englishimgoingtogo",
+   image: "englishimgoingtogo.jpg.jpg",
+   id: "englishimgoingtogo"
+ },
+  {
+   name: "Portugueseand",
+   image: "portugueseand.jpg",
+   id: "portugueseand"
+ },
+ {
+   name: "Englishand",
+   image: "englishand.jpg",
+   id: "englishand"
+ },
+ {
+   name: "Portuguesebread",
+   image: "portuguesebread.jpg",
+   id: "portugueseimgoingtogo"
+ },
+ {
+   name: "Englishbread",
+   image: "englishbread.jpg",
+   id: "englishbread"
+ }
+];
+
+const gameLevels = {
+ easy: {
+   class: "easy",
+   pairs: 6,
+   twoStar: 6,
+   oneStar: 10
+ },
+ medium: {
+   class: "medium",
+   pairs: 8,
+   twoStar: 10,
+   oneStar: 14
+ },
+ hard: {
+   class: "hard",
+   pairs: 12,
+   twoStar: 16,
+   oneStar: 24
+ }
+};
