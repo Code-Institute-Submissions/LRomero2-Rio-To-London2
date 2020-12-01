30336850c576e5410b21b08c20319f103c4b9c7a
LRomero2 <lauren.romero@outlook.com>
2020-12-01 23:35:50 +0000

    Added all jasmine testing code to gitignore and followed the video tutorial at code institute for jasmine testing.

--- testing/scripts/calc.js
+++
@@ -1,7 +0,0 @@
-function addition(numOne, numTwo) {
-    if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo;
-    } else {
-        return "Error!";
-    }
-}
